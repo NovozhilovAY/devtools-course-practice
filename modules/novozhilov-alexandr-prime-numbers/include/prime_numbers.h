f40b4a89984f2c97e9b3aab48016ead10c5bb2d0
NovozhilovAY <sasha.n191@yandex.ru>
2022-04-11 18:31:56 +0300

    add first realization

--- modules/novozhilov-alexandr-prime-numbers/include/prime_numbers.h
+++ modules/novozhilov-alexandr-prime-numbers/include/prime_numbers.h
@@ -3,12 +3,13 @@
 #ifndef MODULES_NOVOZHILOV_ALEXANDR_PRIME_NUMBERS_INCLUDE_PRIME_NUMBERS_H_
 #define MODULES_NOVOZHILOV_ALEXANDR_PRIME_NUMBERS_INCLUDE_PRIME_NUMBERS_H_
 #include<vector>
+#include <stdexcept>
+#include <iostream>
 
 class PrimeNumbers {
 public:
 	static std::vector<int> getPrimeNumbers(int from, int to);
 	static void printPrimeNumbers(int from, int to);
 };
-
 
 #endif  // MODULES_NOVOZHILOV_ALEXANDR_PRIME_NUMBERS_INCLUDE_PRIME_NUMBERS_H_
--- modules/novozhilov-alexandr-prime-numbers/test/test.cpp
+++ modules/novozhilov-alexandr-prime-numbers/test/test.cpp
@@ -3,9 +3,36 @@
 #include <gtest/gtest.h>
 #include "include/prime_numbers.h"
 
-TEST(PrimeNumbersTest, test_test) {
-    PrimeNumbers::getPrimeNumbers(1,2);
-    ASSERT_EQ(0, 0);
+TEST(PrimeNumbersTest, throws_when_incorrect_parameters1) {
+    ASSERT_ANY_THROW(PrimeNumbers::printPrimeNumbers(-2,-5));
 }
 
+TEST(PrimeNumbersTest, throws_when_incorrect_parameters2) {
+    ASSERT_ANY_THROW(PrimeNumbers::printPrimeNumbers(10, 5));
+}
 
+TEST(PrimeNumbersTest, print_prime_numbers_works) {
+    ASSERT_NO_THROW(PrimeNumbers::printPrimeNumbers(4, 10));
+}
+
+TEST(PrimeNumbersTest, get_throws_when_incorrect_parameters1) {
+    ASSERT_ANY_THROW(PrimeNumbers::printPrimeNumbers(-2, -5));
+}
+
+TEST(PrimeNumbersTest, get_throws_when_incorrect_parameters2) {
+    ASSERT_ANY_THROW(PrimeNumbers::printPrimeNumbers(10, 5));
+}
+
+TEST(PrimeNumbersTest, get_prime_numbers_works) {
+    ASSERT_NO_THROW(PrimeNumbers::getPrimeNumbers(4, 10));
+}
+
+TEST(PrimeNumbersTest, get_prime_numbers_returns_correct_result) {
+    std::vector<int> expectedResult;
+    expectedResult.push_back(2);
+    expectedResult.push_back(3);
+    expectedResult.push_back(5);
+    expectedResult.push_back(7);
+    expectedResult.push_back(11);
+    ASSERT_EQ(PrimeNumbers::getPrimeNumbers(0, 11), expectedResult);
+}
