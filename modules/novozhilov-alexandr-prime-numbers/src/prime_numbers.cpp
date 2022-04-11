3cfe5986b7ab2d4822c7bd03022ffe3ec550b93b
NovozhilovAY <sasha.n191@yandex.ru>
2022-04-11 17:42:00 +0300

    init commit

---
+++ modules/novozhilov-alexandr-prime-numbers/src/prime_numbers.cpp
@@ -0,0 +1,10 @@
+#include "..\include\prime_numbers.h"
+
+std::vector<int> PrimeNumbers::getPrimeNumbers(int from, int to)
+{
+	return std::vector<int>();
+}
+
+void PrimeNumbers::printPrimeNumbers(int from, int to)
+{
+}
---
+++ modules/novozhilov-alexandr-prime-numbers/test/test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2022 Novozhilov Alexandr
+
+#include <gtest/gtest.h>
+#include "include/prime_numbers.h"
+
+TEST(PrimeNumbersTest, test_test) {
+    PrimeNumbers::getPrimeNumbers(1,2);
+    ASSERT_EQ(0, 0);
+}
+
+
