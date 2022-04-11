2fdef78cae1deb888a54995efc4311eed247d359
NovozhilovAY <sasha.n191@yandex.ru>
2022-04-11 18:52:04 +0300

    fix slyle

--- modules/novozhilov-alexandr-prime-numbers/include/prime_numbers.h
+++ modules/novozhilov-alexandr-prime-numbers/include/prime_numbers.h
@@ -7,9 +7,9 @@
 #include <iostream>
 
 class PrimeNumbers {
-public:
-	static std::vector<int> getPrimeNumbers(int from, int to);
-	static void printPrimeNumbers(int from, int to);
+ public:
+    static std::vector<int> getPrimeNumbers(int from, int to);
+    static void printPrimeNumbers(int from, int to);
 };
 
 #endif  // MODULES_NOVOZHILOV_ALEXANDR_PRIME_NUMBERS_INCLUDE_PRIME_NUMBERS_H_
